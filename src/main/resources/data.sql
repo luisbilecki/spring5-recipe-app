f93889ff02842515ecf9850b5cae7bff409f8be8
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-11-25 11:43:14 -0300

    feat: added recipes content from db on view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO category (description)
     VALUES ('American'), ('Italian'), ('Mexican'), ('Fast Food');
 INSERT INTO unit_of_measure (description)
-    VALUES ('Teaspoon'), ('Tablespoon'), ('Cup'), ('Pinche'), ('Ounce');
+    VALUES ('Teaspoon'), ('Tablespoon'), ('Cup'), ('Pinche'), ('Ounce'), ('Each'), ('Dash'), ('Pint');
