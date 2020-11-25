1834972d1abc44029bbd4ccbb8fb0990022d5e4a
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-11-25 10:56:28 -0300

    feat: initialized h2 database with seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (description)
+    VALUES ('American'), ('Italian'), ('Mexican'), ('Fast Food');
+INSERT INTO unit_of_measure (description)
+    VALUES ('Teaspoon'), ('Tablespoon'), ('Cup'), ('Pinche'), ('Ounce');
