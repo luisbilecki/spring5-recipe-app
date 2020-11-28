f117d6309da4b964cdacc1f833d2ca848658d086
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-11-28 16:32:45 -0300

    feat: created a method to show any ingredient

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -2,6 +2,7 @@
 package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.services.IngredientService;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.*;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @RequestMapping("/recipe/{recipeId}/ingredients")
@@ -24,5 +27,19 @@ public class IngredientController {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
+    }
+
+    @RequestMapping("/recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id,
+                                       Model model){
+        model.addAttribute(
+                "ingredient",
+                ingredientService.findByRecipeIdAndIngredientId(
+                        Long.valueOf(recipeId),
+                        Long.valueOf(id)
+                ));
+
+        return "recipe/ingredient/show";
     }
 }
